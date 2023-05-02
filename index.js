2eee46aaa3bbc7092fb50bad3107d27af3e112f3
mziyadam <mziyadam@users.noreply.github.com>
2023-05-02 22:24:40 +0700

    new

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var http = require('http');
-var url = require('url');
+// var http = require('http');
+// var url = require('url');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-    res.write(txt);
-    res.end();
-}).listen(8080);
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {
+//       'Content-Type': 'text/html'
+//   });
+//   var q = url.parse(req.url, true).query;
+// var txt = q.year + " " + q.month;
+//   res.write(txt);
+//   res.end();
+// }).listen(8080);
+const express = require('express'); //Import the express dependency
+const app = express();              //Instantiate an express app, the main work horse of this server
+const port = 5000;                  //Save the port number where your server will be listening
+
+//Idiomatic expression in express to route and respond to a client request
+app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+});
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`); 
+});
